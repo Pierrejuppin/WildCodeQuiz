64a52eddcdb39fb3753ad1e7bbffc4c6d7361670
angelique888 <angeliquegrall5@gmail.com>
2024-03-14 17:02:25 +0100

    Timer

--- script.js
+++ script.js
@@ -12,3 +12,28 @@ function openNav(){
 function closeNav(){
     menu.classList.remove("active")
 }
+
+const startTimer = 5
+let timer = startTimer * 60
+
+const timerElement = document.querySelector(".timer")
+
+setInterval(timing, 1000);
+
+function timing () {
+const temps = 0;
+  let minutes = parseInt(temps / 60, 10);
+  let secondes = parseInt(temps % 60, 10);
+
+  minutes = minutes < 10 ? "0" + minutes : minutes
+  secondes = secondes < 10 ? "0" + secondes : secondes
+
+  timerElement.innerText = `${minutes}:${secondes}`
+  temps = temps <= 0 ? 0 : temps - 1
+
+  timerElement.classList.
+
+  console.log(".timer")
+
+}
+
