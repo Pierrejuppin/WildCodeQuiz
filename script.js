a910be8138fbabd8bcab8c2b2d2cfda614effe78
HippolyteWeber <hippolyte.weber@gmail.com>
2024-03-14 17:38:22 +0100

    feat: responsive of home and main page

--- script.js
+++ script.js
@@ -5,10 +5,20 @@ const closeBtn = document.getElementById("closeBtn");
 openBtn.onclick = openNav;
 closeBtn.onclick = closeNav;
 
-function openNav(){
-    menu.classList.add("active");
+function openNav() {
+  menu.classList.add("active");
 }
 
-function closeNav(){
-    menu.classList.remove("active")
+function closeNav() {
+  menu.classList.remove("active");
 }
+
+//ci dessous la couleur de la page à l'arrivé de l'utilisateur sur le site//
+const homeBody = document.getElementById("background");
+const backgroundColorDoc = `#82c0ccff`;
+homeBody.style.backgroundColor = backgroundColorDoc;
+// ci dessous creation des variables pour display none la page du quiz avant selection du pseudo utilisateur//
+const headerDisplay = document.querySelector("header");
+const mainDisplay = document.querySelector("main");
+headerDisplay.style.display = "none";
+mainDisplay.style.display = "none";
